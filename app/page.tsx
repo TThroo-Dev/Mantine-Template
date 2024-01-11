739e853d26174e9e538178373692ccb07e49079a
Ramu Narasinga <ramu@binlo.com.hk>
2024-01-11 23:12:55 +0000

    added mantine components

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
+import { Faq } from "./components/Faq";
+import { Features } from "./components/Features";
+import { Footer } from "./components/Footer";
+import { HeaderMegaMenu } from "./components/Header";
+import Hero from "./components/Hero";
+import { SubscribeForm } from "./components/SubscribeForm";
+
 export default function HomePage() {
-  return <div>Home page</div>;
+  return <div>
+    <HeaderMegaMenu />
+    <Hero />
+    <Features />
+    <Faq />
+    <Footer />
+  </div>;
 }
